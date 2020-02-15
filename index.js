8bfebb6391d3a58092914a2fc546bc592e330ae1
Etriz <ryanpdesigns@gmail.com>
2020-02-14 21:55:26 -0600

    all tests successful

--- index.js
+++ index.js
@@ -69,7 +69,30 @@ Person.prototype.toString = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {}
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
+Car.prototype.drive = function(distance) {
+  // mpg = miles / gallons
+  // mpg * gallons = miles
+  // gallons = miles / mpg
+  if (distance / this.milesPerGallon < this.tank) {
+    // enough fuel
+    this.tank = distance / this.milesPerGallon;
+    this.odometer += distance;
+  } else {
+    //not enough fuel
+    this.tank = 0;
+    this.odometer += distance - 1;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+};
 
 /*
   TASK 3
@@ -78,16 +101,25 @@ function Car() {}
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {}
+function Baby(babyAtt) {
+  Person.call(this, babyAtt);
+  this.age = 5;
+  this.favoriteToy = "trains";
+}
+
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global - binds 'this' to the window element
+  2. Implicit - binds 'this' to the current object when function is used as a method
+  3. New - binds 'this' to the specific version of the objects
+  4. Explicit - defines 'this' when used in the call method
 */
 
 ///////// END OF CHALLENGE /////////
